0c46c6d49b1d9925ec8b2afe728a1f8f968f1005
9999QQQ <vladjagors@gmail.com>
2026-03-19 12:37:10 +0200

    Delete 4_arhivs.ps1

--- 4_arhivs.ps1
+++
@@ -1,8 +0,0 @@
-$downloadsPath = "$env:USERPROFILE\Downloads"
-$zipPath = "$env:USERPROFILE\Documents\PDF_Backup.zip"
-
-$pdfFaili = Get-ChildItem $downloadsPath -Filter *.pdf | Where-Object {
-    $_.LastWriteTime -gt (Get-Date).AddHours(-48)
-}
-
-Compress-Archive -Path $pdfFaili.FullName -DestinationPath $zipPath -Force
