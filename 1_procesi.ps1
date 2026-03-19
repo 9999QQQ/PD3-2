32c17591c24d0291b3b18b0b6cf9809a6c578966
9999QQQ <vladjagors@gmail.com>
2026-03-19 12:19:37 +0200

    Delete 1_procesi.ps1

--- 1_procesi.ps1
+++
@@ -1,8 +0,0 @@
-$visiProcesi = Get-Process
-
-$filtreti = $visiProcesi | Where-Object {
-    ($_.WorkingSet -gt 150MB) -and ($_.Name -ne "msedge")
-}
-
-$filtreti | Select-Object Name, Id, WorkingSet |
-Export-Csv "$env:USERPROFILE\Documents\LielieProcesi.csv" -NoTypeInformation
